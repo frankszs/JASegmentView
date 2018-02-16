a45fb91c3cb166bc0149501fea66401d30cf9a7d
Junaid Ahmed <junaid6121@gmail.com>
2018-02-16 16:10:50 +1300

    Initial Commit

---
+++ FKSegmentView/FKSegmentView.h
@@ -0,0 +1,19 @@
+//
+//  FKSegmentView.h
+//  FKSegmentView
+//
+//  Created by f0dsterz on 16/02/18.
+//  Copyright © 2018 frankszs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FKSegmentView.
+FOUNDATION_EXPORT double FKSegmentViewVersionNumber;
+
+//! Project version string for FKSegmentView.
+FOUNDATION_EXPORT const unsigned char FKSegmentViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FKSegmentView/PublicHeader.h>
+
+
